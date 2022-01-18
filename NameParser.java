4c98bf854ec386e042cbf93abb54ac110cb24c18
Oks123321 <81802094+Oks123321@users.noreply.github.com>
2022-01-18 22:47:25 +0200

    initial commit

---
+++ NameParser.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+import java.lang.String;
+
+class NameParser {
+    public String parse(String[] names) {
+        StringBuilder result = new StringBuilder();
+
+        for (String name : names) {
+            String[] fullName = name.split(" ");
+            String[] firstName = Arrays.copyOf(fullName, fullName.length - 1);
+            String stringName = firstName[0];
+            result.append(stringName).append(", ");
+        }
+
+       String stringResult = String.valueOf(result).trim();
+        char[] charResult = stringResult.toCharArray();
+        if (charResult.length>0){
+
+        char[] newCharResult = Arrays.copyOf(charResult, charResult.length - 1);
+        String finalResult = String.valueOf(newCharResult);
+
+
+        return finalResult;}
+        return stringResult;
+    }
+}
+
+class NameParserTest {
+    public static void main(String[] args) {
+        String[] names = {"John Doe", "Bill Mask", "Nigua Joshua"};
+
+        //John, Bill, Nigua
+        System.out.println(new NameParser().parse(names));
+    }
+}
